ac9743ce6c3f9e97831a76976861fb9879e5a25a
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2022-06-01 23:03:04 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,8 +1,10 @@
 let express = require("express");
 let app = express();
 
+absolutePath = __dirname + "/views/index.html"
+
 app.get("/", function (req, res) {
-  res.send("Hello Express");
+    res.sendFile(absolutePath)
 });
 
 console.log("Hello World");
