538340ceb2106f190dc4a987a39ed94e8e783908
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2022-06-02 01:15:46 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,20 +1,25 @@
 let express = require("express");
 const req = require("express/lib/request");
+require("dotenv").config();
 let app = express();
 
-absolutePath = __dirname + "/views/index.html"
+absolutePath = __dirname + "/views/index.html";
 
-staticServed = express.static(__dirname + "/public")
+staticServed = express.static(__dirname + "/public");
 
-app.use("/public", staticServed)
+app.use("/public", staticServed);
 
 app.get("/", function (req, res) {
-    res.sendFile(absolutePath)
+  res.sendFile(absolutePath);
 });
 
-app.get("/json", function(req, res) {
-    res.json({"message": "Hello json"})
-})
+app.get("/json", function (req, res) {
+  if (process.env.MESSAGE_STYLE == "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  } else {
+    res.json({ message: "Hello json" });
+  }
+});
 
 console.log("Hello World");
 
